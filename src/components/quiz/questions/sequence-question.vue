5a8e6d8693ceee1fca9944dca01febb6e38a99de
Lege19 <111765057+Lege19@users.noreply.github.com>
2023-09-23 09:49:11 +0100

    V0.2.0 - Added Sequence round support

--- src/components/quiz/questions/sequence-question.vue
+++ src/components/quiz/questions/sequence-question.vue
@@ -1,7 +1,53 @@
 <script setup lang="ts">
+import { type Group } from "@/quizJson";
 
+import useQuiz from "@/stores/quiz";
+const quiz = useQuiz();
+import useQuizProgress from "@/stores/quizProgress";
+const quizProgress = useQuizProgress();
+
+import GenericCard from "../generic-card.vue";
+import GroupCation from "../group-caption.vue";
+
+import { computed, watch } from "vue";
+
+const cards = computed(() => {
+    return (quizProgress.questionObj as Group).cards.slice(0, Math.min(quizProgress.questionProgress! + 1, 4));
+});
+
+
+watch(() => quizProgress.questionProgress, (current, prev) => {
+    if (current == 4) {
+        quizProgress.completeQuestion();
+    }
+});
 </script>
 
 <template>
-
+    <div class="container" @click="quizProgress.forward">
+        <TransitionGroup tag="div" id="card-group">
+            <GenericCard v-for="[index, card] in cards.entries()" class="card" :key="index" :card="card"></GenericCard>
+        </TransitionGroup>
+        <Transition name="caption">
+            <GroupCation v-if="quizProgress.questionProgress == 3">{{ (quizProgress.questionObj! as Group).name }}</GroupCation>
+        </Transition>
+    </div>
 </template>
+
+<style scoped>
+#card-group {
+    display: flex;
+    gap: 2.6666666%;
+    width: 100%;
+}
+.card {
+    flex-basis: 23%
+}
+.container {
+    width: 80%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
