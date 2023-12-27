9a104651cb2659f491668c58f9c2ad1d03de6934
Lege19 <111765057+Lege19@users.noreply.github.com>
2023-12-27 18:21:24 +0000

    fixed (removed) broken link to favicon

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,5 @@ import { RouterView } from "vue-router";
 </script>
 
 <template id="app">
-    <link rel="icon" type="image/png" href="./assets/better flag icon.png">
     <RouterView></RouterView>
 </template>
