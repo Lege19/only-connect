c38a1d86e3c6a67b066c46c2c630306861b662eb
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-02 18:13:11 +0000

    Revert "a"
    This reverts commit b7b5e02894a4cc3e54af9d9372c8cea6e9a73da8.

--- src/components/create-sidebar.vue
+++ src/components/create-sidebar.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import SidebarTemplate from "./sidebar/sidebar-template.vue";
 import SidebarItem from "./sidebar/sidebar-item.vue";
-import OpenQuiz from "./sidebar/open-quiz.vue";
 import CreateArchive from "./sidebar/create-archive.vue";
 import ExtractArchive from "./sidebar/extract-archive.vue";
 </script>
@@ -9,9 +8,6 @@ import ExtractArchive from "./sidebar/extract-archive.vue";
 <template>
     <SidebarTemplate>
         <template #alignTop="props">
-            <OpenQuiz></OpenQuiz>
-        </template>
-        <template #alignBottom="props">
             <CreateArchive></CreateArchive>
             <ExtractArchive></ExtractArchive>
         </template>
