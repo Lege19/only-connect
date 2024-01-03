f275a479d7c8746f7b1df4c1e081a81cba2cf037
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-03 17:37:09 +0000

    added editor group

---
+++ src/components/editor/editor-group.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { type Group } from "@/quizJson";
+import { type Ref } from "vue";
+import InputBox from "./input-box.vue";
+const model: Ref<Group|undefined> = defineModel();
+</script>
+
+<template v-if="model">
+    <InputBox v-model="model!.name"></InputBox>
+    <ol>
+        <li v-for="i in model!.cards.keys()">
+            <InputBox v-model="model!.cards[i].data"></InputBox>
+        </li>
+    </ol>
+</template>
