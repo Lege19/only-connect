d07996b7538db81ea4d094da788a23cbdd9428b9
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-03 20:09:21 +0000

    added generic round

---
+++ src/components/editor/generic-round.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import InputBox from "./input-box.vue";
+import GenericQuestion from "./generic-question.vue";
+import { type Ref } from "vue";
+import { type Round } from "@/quizJson";
+const model: Ref<Round|undefined> = defineModel();
+</script>
+
+<template v-if="model">
+    <div>
+        <InputBox v-model="model!.name"></InputBox>
+        <div class="question-container">
+            <GenericQuestion v-for="i in model!.questions.keys()" 
+            v-model="model!.questions[i]" 
+            :round-type="model!.type"></GenericQuestion>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.question-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1em;
+}
+</style>
