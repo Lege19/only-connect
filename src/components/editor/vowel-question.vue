e2101c6c2f55048806b76ca65115203787185612
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-03 18:05:23 +0000

    added vowel and wall questions

---
+++ src/components/editor/vowel-question.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { type VowelQuestion } from "@/quizJson";
+import { type Ref } from "vue";
+import InputBox from "./input-box.vue";
+const model: Ref<VowelQuestion|undefined> = defineModel();
+</script>
+
+<template v-if="model">
+    <InputBox v-model="model!.name"></InputBox>
+    <ol>
+        <li v-for="i in model!.phrases.keys()">
+            <InputBox v-model="model!.phrases[i]"></InputBox>
+        </li>
+    </ol>
+</template>
---
+++ src/components/editor/wall-question.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import EditorGroup from "./editor-group.vue";
+import { type Ref } from "vue";
+import { type Group } from "@/quizJson";
+const model: Ref<Group[]|undefined> = defineModel();
+</script>
+
+<template v-if="model">
+    <div class="wall-container">
+        <EditorGroup v-for="i in model!.keys()" v-model="model![i]"></EditorGroup>
+    </div>
+</template>
+
+<style scoped>
+.wall-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1em;
+}
+</style>
