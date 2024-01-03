cbd4053d81cfa5ec831743886b511f37522afaaf
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-03 17:22:57 +0000

    added input box component for a pre-styled textarea

---
+++ src/components/editor/input-box.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, type Ref, onMounted } from "vue";
+const model: Ref<string|undefined> = defineModel();
+const textarea: Ref<HTMLTextAreaElement|undefined> = ref(undefined);
+function resizeTextarea() {
+    if (!textarea.value) return;
+    textarea.value.style.height = "";
+    textarea.value.style.height = textarea.value.scrollHeight + "px";
+}
+onMounted(resizeTextarea);
+</script>
+
+<template>
+    <textarea rows="1" @input="resizeTextarea" ref="textarea" v-model="model"></textarea>
+</template>
+
+<style scoped>
+textarea {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    resize: none;
+    color: white;
+    font-size: 1em;
+    font-family: arial;
+}
+</style>
