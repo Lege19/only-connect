36dc8f1d1c5089d8e8d38eabfb645543615e6491
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-04 09:10:14 +0000

    added rounds and title to editor

--- src/components/quiz-editor.vue
+++ src/components/quiz-editor.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
+import useQuiz from "@/stores/quiz";
+const quiz = useQuiz();
+import GenericRound from "./editor/generic-round.vue";
+import InputBox from "./editor/input-box.vue";
 </script>
 
-<template>
-    
+<template v-if="quiz.loaded">
+    <div id="editor-container">
+        <InputBox v-model="quiz.json!.name"></InputBox>
+        <ol style="width: 60%">
+            <li v-for="i in quiz.json!.rounds.keys()" style="margin-bottom: 1em">
+                <GenericRound v-model="quiz.json!.rounds[i]"></GenericRound>
+            </li>
+        </ol>
+    </div>
 </template>
+
+<style scoped>
+#editor-container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
