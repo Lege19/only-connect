0b6809edada247a1c997741d74f870938dd40f8e
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-06 18:10:03 +0000

    Added new round button

--- src/components/quiz-editor.vue
+++ src/components/quiz-editor.vue
@@ -3,6 +3,7 @@ import useQuiz from "@/stores/quiz";
 const quiz = useQuiz();
 import GenericRound from "./editor/generic-round.vue";
 import InputBox from "./editor/input-box.vue";
+import { RoundType } from "@/quizJson";
 </script>
 
 <template v-if="quiz.loaded">
@@ -12,6 +13,28 @@ import InputBox from "./editor/input-box.vue";
             <li v-for="i in quiz.json!.rounds.keys()" style="margin-bottom: 1em">
                 <GenericRound v-model="quiz.json!.rounds[i]" :del="() => {quiz.json!.rounds.splice(i, 1)}"></GenericRound>
             </li>
+            <div id="new-round-container">
+                <div class="new-round-button" @click="() => {quiz.json!.rounds.push({
+                    'name': '',
+                    'type': RoundType.Connection,
+                    'questions': []
+                    })}">Connection</div>
+                <div class="new-round-button" @click="() => {quiz.json!.rounds.push({
+                    'name': '',
+                    'type': RoundType.Sequence,
+                    'questions': []
+                    })}">Sequence</div>
+                <div class="new-round-button" @click="() => {quiz.json!.rounds.push({
+                    'name': '',
+                    'type': RoundType.Wall,
+                    'questions': []
+                    })}">Wall</div>
+                <div class="new-round-button" @click="() => {quiz.json!.rounds.push({
+                    'name': '',
+                    'type': RoundType.Vowel,
+                    'questions': []
+                    })}">Vowel</div>
+            </div>
         </ol>
     </div>
 </template>
@@ -22,5 +45,19 @@ import InputBox from "./editor/input-box.vue";
     display: flex;
     align-items: center;
     flex-direction: column;
+}
+#new-round-container {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1em;
+}
+.new-round-button {
+    border: 3px gray dashed;
+    border-radius: 10px;
+    height: 2em;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
 }
 </style>
