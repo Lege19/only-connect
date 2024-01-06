1f2180e40d3f6bd934b7485b26c441b661f81fe4
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-06 17:40:08 +0000

    added "add question" button

--- src/components/editor/generic-round.vue
+++ src/components/editor/generic-round.vue
@@ -2,12 +2,38 @@
 import InputBox from "./input-box.vue";
 import GenericQuestion from "./generic-question.vue";
 import DeleteItem from "./delete-item.vue";
-import { type Ref } from "vue";
-import { type Round } from "@/quizJson";
+import NewItem from "./new-item.vue";
+import { type Ref, triggerRef } from "vue";
+import { RoundType, type Round, CardType, type Card, type Group } from "@/quizJson";
 const model: Ref<Round|undefined> = defineModel();
 defineProps<{
     del: () => void
 }>();
+const blankGroup = {
+    "name": "",
+    "cards": Array(4).fill({
+        "type": CardType.Text,
+        "data": ""
+    }) as [Card, Card, Card, Card]
+};
+function addQuestion() {
+    if (!model.value) return;
+    switch (model.value.type) {
+        case RoundType.Connection:
+        case RoundType.Sequence:
+            model.value.questions.push(blankGroup);
+            break;
+        case RoundType.Wall:
+            model.value.questions.push(Array(4).fill(blankGroup) as [Group, Group, Group, Group]);
+            break;
+        case RoundType.Vowel:
+            model.value.questions.push({
+                "name": "",
+                "phrases": []
+            })
+    }
+}
+function trigger() {triggerRef(model)}
 </script>
 
 <template v-if="model">
@@ -17,9 +43,9 @@ defineProps<{
             </InputBox>
         <GenericQuestion v-for="i in model!.questions.keys()" 
         v-model="model!.questions[i]" 
-        :del="() => {model!.questions.splice(i, 1)}"
+        :del="() => {model!.questions.splice(i, 1); trigger}"
         :round-type="model!.type"></GenericQuestion>
-        
+        <NewItem :func="addQuestion"></NewItem>
     </div>
 </template>
 
