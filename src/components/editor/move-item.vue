156af5192b970c09563c0dd15ab6702d70251938
Lege19 <111765057+Lege19@users.noreply.github.com>
2024-01-07 10:23:52 +0000

    added move item component

---
+++ src/components/editor/move-item.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { type Ref } from "vue";
+const model: Ref<any[]|undefined> = defineModel();
+const props = defineProps<{
+    index: number
+}>();
+function moveUp() {
+    if (!model.value) return;
+    if (props.index === 0) return;
+    [model.value[props.index], model.value[props.index - 1]] = [model.value[props.index - 1], model.value[props.index]];
+}
+function moveDown() {
+    if (!model.value) return;
+    if (props.index === model.value.length - 1) return;
+    [model.value[props.index], model.value[props.index + 1]] = [model.value[props.index + 1], model.value[props.index]];
+}
+</script>
+
+<template>
+    <div>
+        <img src="@/assets/up.svg" @click="moveUp">
+        <img src="@/assets/down.svg" @click="moveDown">
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+}
+img {
+    height: 0.5em;
+}
+</style>
