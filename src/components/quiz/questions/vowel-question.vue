5ede47c3a5ca60d7084cba98b35a6ea08658bb64
Lege19 <xeneon999983@gmail.com>
2024-07-23 11:41:34 +0100

    fixed vowel animation

--- src/components/quiz/questions/vowel-question.vue
+++ src/components/quiz/questions/vowel-question.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { VowelQuestion } from "@/quizTypes";
-import { ref, type Ref, computed, type ComputedRef } from "vue";
-const vowels = ["A", "E", "I", "O", "U", " "];
+import { ref, type Ref } from "vue";
+import cloneDeep from 'lodash.clonedeep'
+const vowels = ["A", "E", "I", "O", "U"];
 
 const props = defineProps<{
     question: VowelQuestion
@@ -21,15 +22,19 @@ type VowelQuestionProgress =
 const progress: Ref<VowelQuestionProgress> = ref({started: false});
 
 function addRandomSpace(text: Letter[]): Letter[] {
+    let out = cloneDeep(text);
     const thirdOfLength = text.length/3;
     const position = Math.floor(thirdOfLength + Math.random() * thirdOfLength);
-    return text.splice(position, 0, {char: " ", key: -1});
+    out.splice(position, 0, {char: " ", key: -1});
+    return out;
 }
 
 function getStrings(phraseIdx: number): {withVowels: Letter[], withoutVowels: Letter[]}{
     const srcText = Array.from(props.question.phrases[phraseIdx].toUpperCase())
     const withVowels = Array.from(srcText.entries()).map(([key, char]: [number, string]) => ({char: char, key: key}))
-    const withoutVowels = addRandomSpace(withVowels.filter((x: Letter) => !vowels.includes(x.char)));
+    const withoutVowels = addRandomSpace(
+        withVowels.filter((x: Letter) => !vowels.includes(x.char) && x.char !== " ")
+    );
     return {withVowels, withoutVowels};
 }
 function forward() {
@@ -76,12 +81,12 @@ function back() {
     >
         <Transition name="fade" mode="out-in">
             <div v-if="progress.started" class="vowel-container" :key="progress.phraseIdx">
-                <TransitionGroup tag="h1" name="vowel" style="white-space: pre">
+                <TransitionGroup tag="div" name="vowel" style="display: flex">
                     <template
-                        v-for="letter in (progress.showVowels ? progress.withoutVowels : progress.withVowels)"
+                        v-for="letter in (progress.showVowels ? progress.withVowels : progress.withoutVowels)"
                         :key="letter.key"
                     >
-                        <span>{{ letter.char }}</span>
+                        <h1>{{ letter.char }}</h1>
                     </template>
                 </TransitionGroup>
             </div>
@@ -90,7 +95,7 @@ function back() {
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .vowel-container {
     display: flex;
     justify-content:center;
