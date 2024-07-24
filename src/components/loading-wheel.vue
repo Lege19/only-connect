1245a90550938747ce48034e749909d44b12b94d
Lege19 <xeneon999983@gmail.com>
2024-07-24 14:52:44 +0100

    added loading-wheel

---
+++ src/components/loading-wheel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="loading-wheel">
+        <img src="@/assets/images/only-connect-wheel-outer.svg" class="outer">
+        <img src="@/assets/images/only-connect-wheel-inner.svg" class="inner">
+        <img src="@/assets/images/only-connect-wheel-c.svg" class="c">
+    </div>
+</template>
